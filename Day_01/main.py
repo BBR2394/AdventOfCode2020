635a94060a3a3e13d40c4b899f6dd255a600635c
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2020-12-03 00:34:22 +0100

    :construction: I have the second start to do ^^

--- Day_01/main.py
+++ Day_01/main.py
@@ -3,6 +3,29 @@
 
 import sys
 
+def findTwentyTwentyForSecondStart(lst):
+    if lst[0] == '\n':
+        return 0
+    found = False
+    elem = lst.pop(0)
+    result = []
+    #for len(lst) > 0:
+    print("LOG: findTwentyTwenty : elem = ", elem)
+    lst.pop(len(lst)-1)
+    while len(lst) > 0:
+        ##here i have to add code
+        for i in lst:
+            print("elem + i = ", int(elem), " ", int(i), " = ", int(elem)+int(i))
+            if int(elem) + int(i) == 2020:
+               print("result are : ", elem, " and ", i)
+               found = True
+               result.append(int(elem))
+               result.append(int(i))
+               break
+        if found:
+            return result
+        elem = lst.pop(0)
+        
 def findTwentyTwenty(lst):
     if lst[0] == '\n':
         return 0
@@ -26,8 +49,6 @@ def findTwentyTwenty(lst):
         elem = lst.pop(0)
     
     
-
-
 def main(arg):
     print("Here is where the magik happen")
     if len(arg) >= 2:
